af66d60fdf4e420f4cb3b458bccf3b41e65590d3
Justin Hu <jh2625@cornell.edu>
2017-09-16 01:57:05 -0400

    working on ui

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 
- 
+class testHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('public/index.html')
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('public/index.html')
@@ -11,6 +14,7 @@ class MainHandler(tornado.web.RequestHandler):
 def main():
     application = tornado.web.Application([
         (r"/", MainHandler),
+        (r"/hello", testHandler),
         (r'/public/(.*)', tornado.web.StaticFileHandler, {'path': 'public/'}),
     ])
     http_server = tornado.httpserver.HTTPServer(application)
