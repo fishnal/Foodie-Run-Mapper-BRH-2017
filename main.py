6ea2613f5a4c1eddec2ed12e3f1c3dd6fd123478
Karthik <gvkarthik93@gmail.com>
2017-09-16 00:26:31 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+ 
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('public/index.html')
+
+def main():
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+        (r'/public/(.*)', tornado.web.StaticFileHandler, {'path': 'public/'}),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    port = int(os.environ.get("PORT", 8000))
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+ 
+if __name__ == "__main__":
+    main()
