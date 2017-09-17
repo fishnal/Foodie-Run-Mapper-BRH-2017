c8633ce4ff90760f7d8cff89279ddac0ed469fab
Justin Hu <jh2625@cornell.edu>
2017-09-16 21:14:50 -0400

    rendering json

--- public/yelp.js
+++ public/yelp.js
@@ -1,19 +1,39 @@
 
-var params = {"terms":"deli",
-    "latitude":37.786882,
-    "longitude":-122.399972,
-    };
-var baseurl = "https://api.yelp.com/v3/businesses/search?";
+var params = {
+    "lat_start":40.732848,
+    "long_start":-74.008434,
+    "lat_end":40.722753,
+    "long_end":-73.996233,
+    "price_levels":[1,2,3,4]
+};
+//var baseurl = "https://api.yelp.com/v3/businesses/search?";
+var baseurl ="http://localhost:8000/api?"
 var query =baseurl+$.param(params);
 var token ="O68RjH5EXT7RnWLL1DbfliT4wgFh6ZrsWDNAWk9N7OVEoJHtAPYDmh5klTL5TVhi4N09YQP3DgoOvArWs_Po2p3E5SSMnRG1uiGOAbatqRAk7Lmebmrg5Ieb8WO9WXYx";
 
-$.ajax({
-  url : query,
-    dataType : "json",
-    headers: {
-        "Content-Type":"application/json",
-        "Accept": "application/json",
-        "Authorization": "Bearer " + token},
-    type : 'GET',
-    contentType: "application/json",
+var template = "Restaurant: <ul>{{#businesses}}<li>Name:{{name}};</li>{{/businesses}}</ul>";
+
+function renderData(data){
+    var html = Mustache.to_html(template, data);
+    $('#output').html(html);
+}
+
+$(document).ready(() => {
+   $.ajax({
+      url : query,
+        dataType : "json",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept": "application/json"},
+        type : 'GET',
+        contentType: "application/json",
+        success:function(data) {
+              renderData(data);
+            },
+        error: function (request, status, error) {
+            alert(request.responseText);
+    }
+    });
+
+
 });
